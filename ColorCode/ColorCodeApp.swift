a6cc4f902610d4b9d962aa9246ce2aef2ece2d43
Arbaz Kaladiya <kaladiya.arbaz@gmail.com>
2025-07-31 14:18:31 +0530

    Initial Commit

---
+++ ColorCode/ColorCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorCodeApp.swift
+//  ColorCode
+//
+//  Created by Arbaz Kaladiya on 31/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ColorCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorCodeTests/ColorCodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ColorCodeTests.swift
+//  ColorCodeTests
+//
+//  Created by Arbaz Kaladiya on 31/07/25.
+//
+
+import Testing
+@testable import ColorCode
+
+struct ColorCodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
